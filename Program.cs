d8617e8848857afa3ff4a6afe287b66141fe8a74
zfali <zfali@DESKTOP-M56GFNV>
2024-02-06 11:56:56 +0000

    error removed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+double totalValue = 0;
+string message = " ";
+
+foreach (string value in values)
+
+{
+    if (double.TryParse(value, out double numericValue))
+
+    {
+        totalValue += numericValue;
+        Console.WriteLine("Total:" + totalValue);
+    }
+
+    else
+    {
+        message += value;
+        Console.WriteLine("Message: " + message);
+    }
+}
